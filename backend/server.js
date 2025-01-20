48515c70a6f4606040883f691c83a407c2d26288
vedantk04 <125801012+vedantk04@users.noreply.github.com>
2025-01-20 21:01:49 +0530

    Update server.js
    updated cors

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,7 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 // middleware
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
