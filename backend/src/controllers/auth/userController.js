3f7af5d2925c93b7009d4eff2477b3b943e037e0
vedantk04 <125801012+vedantk04@users.noreply.github.com>
2025-01-20 21:37:35 +0530

    Update userController.js

--- backend/src/controllers/auth/userController.js
+++ backend/src/controllers/auth/userController.js
@@ -47,7 +47,7 @@ export const registerUser = asyncHandler(async (req, res) => {
     path: "/",
     httpOnly: true,
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-    sameSite: "none", // cross-site access --> allow all third-party cookies
+    sameSite: "lax", // cross-site access --> allow all third-party cookies
     secure: false,
   });
 
